67f0864e54b0123c7da82161c564b925306fc4a9
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-02-13 15:43:03 +0530

    Testing AI Code Review Bot5

---
+++ 3_test/test.js
@@ -0,0 +1,11 @@
+app.delete("/teas/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const tea = teaData.find(tea => tea.id == id)
+    if(tea){
+        teaData = teaData.filter(tea => tea.id != id)
+        res.status(200).send({message : "Tea deleted"})
+    }else{
+        res.status(404).send({message : "Tea not found"})
+    }
+    });
+hi kjbwrjvsb 
