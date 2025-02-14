a80526e2469ace3fa3493428ee55ad934bc624cb
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-02-14 09:06:47 +0530

    sec

--- )4_testing/testing.js
+++ )4_testing/testing.js
@@ -7,13 +7,6 @@ import { savePRRev
 /**
  * GitHub Webhook Handler
  */
-export function githubWebhookHandler(req, res) {
-    const signature = `sha256=${crypto
-        .createHmac("sha256", process.env.GITHUB_SECRET)
-        .update(JSON.stringify(req.body))
-        .digest("hex")}`;
-
-    if (req.headers["x-hub-signature-256"] !== signature) {
-        return res.status(401).send("Unauthorized");
+export function githubWebhookd("Unauthorized");
     }
 
