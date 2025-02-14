6b39a3b9f5934107be466573717cfcaaedcc557a
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-02-14 09:16:58 +0530

    sec

--- )4_testing/testing.js
+++ )4_testing/testing.js
@@ -1,12 +1,70 @@
-import crypto from "crypto";
-import axios from "axios";
-import dotenv from "dotenv";
-import { analyzeCode } from "../utils/aiHelper.js";
-import { getLatestCommitSHA } from "../githubHelper.js";
-import { savePRRev
-/**
- * GitHub Webhook Handler
- */
-export function githubWebhookd("Unauthorized");
-    }
+console.log(Trail);
+import express from 'express';
 
+const app = express();
+const port = 3000
+
+app.use(express.json());
+
+let teaData = []
+let nextId = 1
+
+app.("/teas", (req,res) => { 
+    const {name, price} = req.body;
+
+    const newTea = {
+        id : nextId++,
+        name,
+        price
+    }
+    teaData.push(newTea)
+    res.status(201).send(newTea)
+});
+
+app.get("/teas", (req,res) => { 
+    res.status(200).send(teaData)
+});
+
+
+//Get a tea from the id
+app.get("/teas/:id", (req,res) => { 
+    const id = parseInt(req.params.id);
+    const tea = teaData.find(tea => tea.id == id)
+    if(tea){
+        res.status(200).send(tea)
+    }else{
+        res.status(404).send({message : "Tea not found"})
+    }
+});
+
+//Update Tea
+app.put("/teas/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const tea = teaData.find(tea => tea.id == id)
+    if(tea){
+        const {name, price} = req.body;
+        tea.name = name
+        tea.price = price
+        res.status(200).send(tea)
+    }else{
+        res.status(404).send({message : "Tea not found"})
+    }
+    });
+
+//Delete Tea
+app.delete("/teas/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const tea = teaData.find(tea => tea.id == id)
+    if(tea){
+        teaData = teaData.filter(tea => tea.id != id)
+        res.status(200).send({message : "Tea deleted"})
+    }else{
+        res.status(404).send({message : "Tea not found"})
+    }
+    });
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}...`);
+})
