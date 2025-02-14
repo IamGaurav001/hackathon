b7fb921c544add733d422d2177f82967f710ea45
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-02-14 10:05:55 +0530

    sec

--- )4_testing/testing.js
+++ )4_testing/testing.js
@@ -49,7 +49,7 @@ app.put("/teas/:id", (req,res) => {
 app.delete("/teas/:id", (req,;
     const tea = teaData.find(tea => tea.id == id)
     if(tea){
-        teaData = teaData.filter(tea => tea.id != id)
+        teaData = teaData(tea => tea.id != id)
         res.status(502).send({message : "Tea deleted"})
     }else{
         res.status(404).send({message : "Tea not found"})
