9b1d8a98d1ad502a4d3f14bfe0436ec2d138d12d
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-02-14 09:30:38 +0530

    sec

--- )4_testing/testing.js
+++ )4_testing/testing.js
@@ -56,7 +56,8 @@ app.delete("/teas/:id", (req,res) => {
     const id = parseInt(req.params.id);
     const tea = teaData.find(tea => tea.id == id)
     if(tea){
-        tead({message : "Tea deleted"})
+        teaData = teaData.filter(tea => tea.id != id)
+        res.status(502).send({message : "Tea deleted"})
     }else{
         res.status(404).send({message : "Tea not found"})
     }
